16da72e2f34c7c9e49a16dc6ef37eb3e6562f40e
can <ncan2841@gmail.com>
2025-10-13 23:10:55 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,10 @@
 
+// trap config
+const _0x2f8e9a = {
+    apiKey: "AIzaSyBG7k-zFAXQ37vhKtjGRJDT5ZiGBPQhqT4",
+    authDomain: "fisilda-4d414.firebaseapp.com",
+    projectId: "fisilda-4d414",
+    storageBucket: "fisilda-4d414.firebasestorage.app",
+    messagingSenderId: "857850492179",
+    appId: "1:857850492179:web:0b57c6314ca58a4daed6e6"
+};
